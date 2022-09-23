a864c7cf0a1c6cd69945bae95df70e93f7bf5081
Phuong Vo <phuong.vo@tomosia.com>
2022-09-23 09:57:59 +0700

    Push new contract

---
+++ contracts/NewInterfaceNFT.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+interface IMarketItem {
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        MarketItemStatus status;
+    }
+
+    enum MarketItemStatus {
+        Active,
+        Sold,
+        Cancelled
+    }
+}
+
+interface INFTSupport {
+    function addNFTSupportAddress(address nftAddress_) external;
+    function isNFTAddressSupported(address nftAddress_) external view returns (bool);
+}
+
+interface IMigration is INFTSupport, IMarketItem {
+    function transferMarketItem(address nftAddress, MarketItem memory item) external returns (bool);
+}
--- w3-deploy.js
+++ w3-deploy.js
@@ -1,22 +1,26 @@
-export default async function deploy(contractName, args) {
+function getEnviroment() {
+    const dotenv = require("dotenv");
+    dotenv.config();
+    return process.env;
+}
+
+async function deploy(contractName, args) {
     console.log(`Start Deploying ${contractName}`);
-    const Truffle = require("./truffle-config");
     const Web3 = require("web3");
-    const web3 = new Web3(Truffle.networks.goerli.provider);
+    const { PROJECT_ID } = getEnviroment();
+    const provider = `https://goerli.infura.io/v3/${PROJECT_ID}`;
+    const web3Provider = new Web3.providers.HttpProvider(provider);
+    const web3 = new Web3(web3Provider);
     const metadata = require(`./build/contracts/${contractName}.json`);
-    const accounts = await web3.eth.getAccounts();
     const contract = new web3.eth.Contract(metadata.abi);
 
-    const newContractInstance = await contract
-        .deploy({
-            data: metadata.bytecode,
-            arguments: args,
-        })
-        .send({
-            from: accounts[0],
-            gas: 1500000,
-        });
+    contract.deploy({
+        data: metadata.bytecode,
+        arguments: args,
+    });
 
     provider.engine.stop();
     console.log(`End Deploying ${contractName}`);
 }
+
+deploy("Marketplace", null);
